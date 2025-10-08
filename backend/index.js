9e2b2ecbd715b8154fa4a79f61a291e8d326684f
althaf803 <althumohammad803@gmail.com>
2025-10-08 10:55:39 +0530

    ccc

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-//connectDB().then(() => {
+connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-//});
+});
